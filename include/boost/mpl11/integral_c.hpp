a42766ce357736758c8267a9fd61570186885a93
Louis Dionne <ldionne.2@gmail.com>
2013-06-13 12:44:10 -0400

    Add unit tests for integral_c.

--- include/boost/mpl11/integral_c.hpp
+++ include/boost/mpl11/integral_c.hpp
@@ -6,48 +6,38 @@
 #ifndef BOOST_MPL11_INTEGRAL_C_HPP
 #define BOOST_MPL11_INTEGRAL_C_HPP
 
+#include <boost/mpl11/intrinsic/next.hpp>
+#include <boost/mpl11/intrinsic/prior.hpp>
+
 #include <boost/type_traits/remove_cv.hpp>
 
 
 namespace boost { namespace mpl11 { inline namespace v2 {
-namespace extension {
-    template <typename> struct next_impl;
-    template <typename> struct prior_impl;
-}
-
-namespace integral_c_detail { struct tag; }
-
 template <typename I, I N>
-struct integral_c {
-    struct mpl11 { using tag = integral_c_detail::tag; };
+class integral_c {
+    struct implementation {
+        template <template <typename ...> class Impl, typename ...Args>
+        struct apply;
+
+        template <typename IntegralC>
+        struct apply<next, IntegralC> {
+            using type = integral_c<I, N + 1>;
+        };
+
+        template <typename IntegralC>
+        struct apply<prior, IntegralC> {
+            using type = integral_c<I, N - 1>;
+        };
+    };
+
+public:
+    struct mpl11 { using dispatcher = implementation; };
 
     using value_type = typename remove_cv<I>::type;
     static constexpr value_type value = N;
     using type = integral_c;
-
-private:
-    friend struct extension::next_impl<integral_c_detail::tag>;
-    friend struct extension::prior_impl<integral_c_detail::tag>;
-    template <I NN> using rebind = integral_c<I, NN>;
+    constexpr operator value_type() const { return value; }
 };
-
-namespace extension {
-template <>
-struct next_impl<integral_c_detail::tag> {
-    template <typename IntegralC>
-    struct apply {
-        using type = typename IntegralC::template rebind<IntegralC::value + 1>;
-    };
-};
-
-template <>
-struct prior_impl<integral_c_detail::tag> {
-    template <typename IntegralC>
-    struct apply {
-        using type = typename IntegralC::template rebind<IntegralC::value - 1>;
-    };
-};
-}
 }}}
 
 #endif // !BOOST_MPL11_INTEGRAL_C_HPP
