14bcec330bc03b86d47456c9beb414bf388afe12
Louis Dionne <ldionne.2@gmail.com>
2013-06-17 14:18:13 -0400

    Add the is_named_argument trait and remove useless unit tests.

---
+++ include/boost/mpl11/trait/is_named_argument.hpp
@@ -0,0 +1,24 @@
+/*!
+ * @file
+ * This file defines `boost::mpl11::trait::is_named_argument`.
+ */
+
+#ifndef BOOST_MPL11_TRAIT_IS_NAMED_ARGUMENT_HPP
+#define BOOST_MPL11_TRAIT_IS_NAMED_ARGUMENT_HPP
+
+#include <boost/mpl11/has_xxx.hpp>
+
+
+namespace boost { namespace mpl11 { inline namespace v2 { namespace trait {
+    namespace is_named_argument_detail {
+        BOOST_MPL11_HAS_NESTED_TYPE(has_is_named_argument,
+                                    mpl11::is_named_argument)
+    }
+
+    template <typename T>
+    struct is_named_argument
+        : is_named_argument_detail::has_is_named_argument<T>::type
+    { };
+}}}}
+
+#endif // !BOOST_MPL11_TRAIT_IS_NAMED_ARGUMENT_HPP
