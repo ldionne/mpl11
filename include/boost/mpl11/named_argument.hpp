35f393d8b3525ec6de96ff56f11d8dcddd6f318b
Louis Dionne <ldionne.2@gmail.com>
2013-06-17 14:19:55 -0400

    Add boost::mpl11::named_argument.

---
+++ include/boost/mpl11/named_argument.hpp
@@ -0,0 +1,19 @@
+/*!
+ * @file
+ * This file defines `boost::mpl11::named_argument`.
+ */
+
+#ifndef BOOST_MPL11_NAMED_ARGUMENT_HPP
+#define BOOST_MPL11_NAMED_ARGUMENT_HPP
+
+namespace boost { namespace mpl11 { inline namespace v2 {
+    template <typename Name, typename Value>
+    struct named_argument {
+        struct mpl11 { struct is_named_argument; };
+
+        using key = Name;
+        using value = Value;
+    };
+}}}
+
+#endif // !BOOST_MPL11_NAMED_ARGUMENT_HPP
