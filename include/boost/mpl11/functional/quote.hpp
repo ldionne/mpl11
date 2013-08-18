097ef7db71f7518f769b749efed1fa6ace2e3d62
Louis Dionne <ldionne.2@gmail.com>
2013-08-18 16:41:59 -0400

    Support non-metafunctions in `quote`.

--- include/boost/mpl11/functional/quote.hpp
+++ include/boost/mpl11/functional/quote.hpp
@@ -6,12 +6,22 @@
 #ifndef BOOST_MPL11_FUNCTIONAL_QUOTE_HPP
 #define BOOST_MPL11_FUNCTIONAL_QUOTE_HPP
 
+#include <boost/mpl11/has_xxx.hpp>
+#include <boost/mpl11/identity.hpp>
+#include <boost/mpl11/if.hpp>
+
+
 namespace boost { namespace mpl11 {
+    namespace quote_detail { BOOST_MPL11_HAS_NESTED_TYPE(has_type, type) }
+
     template <template <typename ...> class M>
     struct quote {
         template <typename ...Args>
         struct apply
-            : M<Args...>
+            : if_<quote_detail::has_type<M<Args...>>,
+                M<Args...>,
+                identity<M<Args...>>
+            >::type
         { };
     };
 }} // end namespace boost::mpl11
