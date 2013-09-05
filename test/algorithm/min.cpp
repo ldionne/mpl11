3575d46f588bbeb8523ff65cf7b12a973687e718
Louis Dionne <ldionne.2@gmail.com>
2013-09-04 22:13:20 -0400

    Add unit tests for `min` and `max`.

---
+++ test/algorithm/min.cpp
@@ -0,0 +1,27 @@
+/*!
+ * @file
+ * Contains unit tests for `boost::mpl11::algorithm::min`.
+ */
+
+#include <boost/mpl11/algorithm/min.hpp>
+
+#include <boost/mpl11/integral_c.hpp>
+#include <boost/mpl11/intrinsic/equal_to.hpp>
+
+
+using namespace boost::mpl11;
+using algorithm::min;
+using intrinsic::equal_to;
+
+static_assert(equal_to<
+    min<int_<1>, int_<0>>::type,
+    int_<0>
+>::type::value, "");
+
+static_assert(equal_to<
+    min<int_<0>, int_<-3>, int_<2>>::type,
+    int_<-3>
+>::type::value, "");
+
+
+int main() { }
