2f53b6425aa2f6930c78220fb5217e5c4e0053a3
Louis Dionne <ldionne.2@gmail.com>
2013-10-13 17:30:55 -0400

    Reimplement view::empty without view::bounded_by.

--- include/boost/mpl11/view/empty.hpp
+++ include/boost/mpl11/view/empty.hpp
@@ -6,29 +6,40 @@
 #ifndef BOOST_MPL11_VIEW_EMPTY_HPP
 #define BOOST_MPL11_VIEW_EMPTY_HPP
 
+#include <boost/mpl11/categories.hpp>
 #include <boost/mpl11/dispatch.hpp>
-#include <boost/mpl11/integral_c.hpp>
+#include <boost/mpl11/identity.hpp>
 #include <boost/mpl11/intrinsic/begin_fwd.hpp>
 #include <boost/mpl11/intrinsic/category_of_fwd.hpp>
 #include <boost/mpl11/intrinsic/end_fwd.hpp>
-#include <boost/mpl11/view/bounded_by.hpp>
 #include <boost/mpl11/view/empty_fwd.hpp>
 
 
 namespace boost { namespace mpl11 {
+    namespace empty_view_detail { struct iterator; }
+
+    template <>
+    struct dispatch<tag::category_of, empty_view_detail::iterator>
+        : identity<
+            category::random_access_iterator
+        >
+    { };
+
     template <>
     struct dispatch<tag::begin, view::empty>
-        : begin<view::bounded_by<uint<0>, uint<0>>>
+        : identity<empty_view_detail::iterator>
     { };
 
     template <>
     struct dispatch<tag::end, view::empty>
-        : end<view::bounded_by<uint<0>, uint<0>>>
+        : identity<empty_view_detail::iterator>
     { };
 
     template <>
     struct dispatch<tag::category_of, view::empty>
-        : category_of<view::bounded_by<uint<0>, uint<0>>>
+        : identity<
+            category::random_access_sequence
+        >
     { };
 }} // end namespace boost::mpl11
 
