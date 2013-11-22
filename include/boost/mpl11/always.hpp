3a2d1b842d32f046c7f341e1c654f0a3cfdf7dea
Louis Dionne <ldionne.2@gmail.com>
2013-11-21 21:06:36 -0500

    Use identity in always' implementation.

--- include/boost/mpl11/always.hpp
+++ include/boost/mpl11/always.hpp
@@ -8,14 +8,14 @@
 
 #include <boost/mpl11/fwd/always.hpp>
 
+#include <boost/mpl11/identity.hpp>
+
 
 namespace boost { namespace mpl11 {
     template <typename T>
     struct always {
         template <typename ...>
-        struct apply {
-            using type = T;
-        };
+        using apply = identity<T>;
     };
 }} // end namespace boost::mpl11
 
