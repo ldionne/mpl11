27c43f3183809f3d9672be9a42cad4ad532417dc
Louis Dionne <ldionne.2@gmail.com>
2013-12-08 18:33:29 -0500

    Optimize replicate.

--- include/boost/mpl11/replicate.hpp
+++ include/boost/mpl11/replicate.hpp
@@ -8,66 +8,112 @@
 
 #include <boost/mpl11/fwd/replicate.hpp>
 
-#include <boost/mpl11/forward_iterator.hpp>
+#include <boost/mpl11/detail/dependent_on.hpp>
+#include <boost/mpl11/detail/std_size_t.hpp>
+#include <boost/mpl11/fwd/advance.hpp>
+#include <boost/mpl11/fwd/begin.hpp>
 #include <boost/mpl11/fwd/class_of.hpp>
+#include <boost/mpl11/fwd/deref.hpp>
+#include <boost/mpl11/fwd/distance.hpp>
+#include <boost/mpl11/fwd/end.hpp>
+#include <boost/mpl11/fwd/equal.hpp>
+#include <boost/mpl11/fwd/has_optimization.hpp>
+#include <boost/mpl11/fwd/is_empty.hpp>
+#include <boost/mpl11/fwd/next.hpp>
+#include <boost/mpl11/fwd/not_equal.hpp>
+#include <boost/mpl11/fwd/size.hpp>
 #include <boost/mpl11/fwd/unpack.hpp>
 #include <boost/mpl11/integral_c.hpp> // required by fwd/replicate.hpp
 #include <boost/mpl11/join.hpp>
-#include <boost/mpl11/sequence.hpp>
+#include <boost/mpl11/optimization.hpp>
 #include <boost/mpl11/vector.hpp>
 
 
 namespace boost { namespace mpl11 {
+//////////////////////////////////////////////////////////////////////////////
+// Replicate iterator
+//////////////////////////////////////////////////////////////////////////////
 namespace replicate_detail {
-    template <unsigned long long N, typename T>
-    struct replicate_iterator;
+    template <detail::std_size_t N, typename T>
+    struct iterator;
+}
 
-    struct replicate_iterator_class final : ForwardIterator {
-        template <typename Iterator>             struct next_impl;
-        template <typename Iterator>             struct deref_impl;
-        template <typename Self, typename Other> struct equal_impl;
+/////////////////////////////////
+// ForwardIterator
+/////////////////////////////////
+template <typename T>
+struct next<replicate_detail::iterator<0, T>> {
+    struct type;
 
-        template <typename T> struct next_impl<replicate_iterator<0, T>>;
-        template <typename T> struct deref_impl<replicate_iterator<0, T>>;
+    static_assert(detail::dependent_on<type>::template value<bool, false>(),
+    "Invalid usage of `next`: "
+    "Incrementing a past-the-end iterator of `replicate`.");
+};
 
-        template <unsigned long long N, typename T>
-        struct next_impl<replicate_iterator<N, T>> {
-            using type = replicate_iterator<N-1, T>;
-        };
+template <detail::std_size_t N, typename T>
+struct next<replicate_detail::iterator<N, T>> {
+    using type = replicate_detail::iterator<N-1, T>;
+};
 
-        template <unsigned long long N, typename T>
-        struct deref_impl<replicate_iterator<N, T>> {
-            using type = T;
-        };
+template <typename T>
+struct deref<replicate_detail::iterator<0, T>> {
+    struct type;
 
-        template <unsigned long long I, unsigned long long J, typename T>
-        struct equal_impl<replicate_iterator<I, T>, replicate_iterator<J, T>>
-            : bool_<I == J>
-        { };
+    static_assert(detail::dependent_on<type>::template value<bool, false>(),
+    "Invalid usage of `next`: "
+    "Dereferencing a past-the-end iterator of `replicate`.");
+};
+
+template <detail::std_size_t N, typename T>
+struct deref<replicate_detail::iterator<N, T>> {
+    using type = T;
+};
+
+template <detail::std_size_t I, detail::std_size_t J, typename T>
+struct equal<
+    replicate_detail::iterator<I, T>,
+    replicate_detail::iterator<J, T>
+>
+    : bool_<I == J>
+{ };
+
+template <detail::std_size_t I, detail::std_size_t J, typename T>
+struct not_equal<
+    replicate_detail::iterator<I, T>,
+    replicate_detail::iterator<J, T>
+>
+    : bool_<I != J>
+{ };
+
+template <detail::std_size_t N, typename T, typename Dist>
+struct advance<replicate_detail::iterator<N, T>, Dist> {
+    static_assert(Dist::value <= N,
+    "Invalid usage of `advance`: "
+    "Advancing a `replicate` iterator beyond its end.");
+
+    using type = replicate_detail::iterator<N - Dist::value, T>;
+};
+
+template <detail::std_size_t I, detail::std_size_t J, typename T>
+struct distance<
+    replicate_detail::iterator<I, T>,
+    replicate_detail::iterator<J, T>
+>
+    : integral_c<decltype(I - J), I - J>
+{ };
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Replicate
+//////////////////////////////////////////////////////////////////////////////
+namespace replicate_detail {
+    template <typename N>
+    struct assert_usage {
+        static_assert(N::value >= 0,
+        "Invalid usage of `replicate`: `N::value` must be non-negative.");
     };
 
-    struct replicate_class final : Sequence {
-        template <typename Replicate> struct begin_impl;
-        template <typename Replicate> struct end_impl;
-
-        template <typename N, typename T>
-        struct begin_impl<replicate<N, T>> {
-            static_assert(N::value >= 0,
-            "Invalid usage of replicate: N::value must be non-negative.");
-
-            using type = replicate_iterator<N::value, T>;
-        };
-
-        template <typename N, typename T>
-        struct end_impl<replicate<N, T>> {
-            static_assert(N::value >= 0,
-            "Invalid usage of replicate: N::value must be non-negative.");
-
-            using type = replicate_iterator<0, T>;
-        };
-    };
-
-    template <unsigned long long N, typename T>
+    template <detail::std_size_t N, typename T>
     struct replicate_impl
         : join<
             typename replicate_impl<N/2, T>::type,
@@ -82,27 +128,48 @@ namespace replicate_detail {
     struct replicate_impl<1, T> { using type = vector<T>; };
 } // end namespace replicate_detail
 
-template <unsigned long long N, typename T, typename Default>
-struct class_of<replicate_detail::replicate_iterator<N, T>, Default> {
-    using type = replicate_detail::replicate_iterator_class;
-};
+template <typename N, typename T>
+struct replicate
+    : replicate_detail::assert_usage<N>,
+      replicate_detail::replicate_impl<N::value, T>
+{ };
 
-template <typename N, typename T, typename Default>
-struct class_of<replicate<N, T>, Default> {
-    using type = replicate_detail::replicate_class;
+/////////////////////////////////
+// Sequence
+/////////////////////////////////
+template <typename N, typename T>
+struct begin<replicate<N, T>> : replicate_detail::assert_usage<N> {
+    using type = replicate_detail::iterator<N::value, T>;
 };
 
 template <typename N, typename T>
-struct replicate {
-    static_assert(N::value >= 0,
-    "Invalid usage of replicate: N::value must be non-negative.");
-
-    using type = typename replicate_detail::replicate_impl<N::value, T>::type;
+struct end<replicate<N, T>> : replicate_detail::assert_usage<N> {
+    using type = replicate_detail::iterator<0, T>;
 };
 
+template <typename N, typename T>
+struct is_empty<replicate<N, T>>
+    : replicate_detail::assert_usage<N>,
+      bool_<N::value == 0>
+{ };
+
+template <typename N, typename T>
+struct size<replicate<N, T>>
+    : replicate_detail::assert_usage<N>,
+      N
+{ };
+
+/////////////////////////////////
+// Other specializations
+/////////////////////////////////
 template <typename N, typename T, typename F>
 struct unpack<replicate<N, T>, F>
-    : unpack<replicate_t<N, T>, F>
+    : unpack<typename replicate<N, T>::type, F>
+{ };
+
+template <typename N, typename T>
+struct has_optimization<replicate<N, T>, optimization::O1_size>
+    : true_
 { };
 }} // end namespace boost::mpl11
 
--- test/replicate.cpp
+++ test/replicate.cpp
@@ -6,6 +6,12 @@
 #include <boost/mpl11/replicate.hpp>
 
 #include <boost/mpl11/equal.hpp>
+#include <boost/mpl11/has_optimization.hpp>
+#include <boost/mpl11/into.hpp>
+#include <boost/mpl11/is_empty.hpp>
+#include <boost/mpl11/optimization.hpp>
+#include <boost/mpl11/size.hpp>
+#include <boost/mpl11/unpack.hpp>
 #include <boost/mpl11/vector.hpp>
 
 
@@ -17,6 +23,21 @@ struct test_replicate {
     struct equals {
         static_assert(equal<vector<U...>, replicate_c<N, T>>::value, "");
         static_assert(equal<vector<U...>, replicate_c_t<N, T>>::value, "");
+
+        static_assert(is_empty<replicate_c<N, T>>::value == (sizeof...(U) == 0), "");
+        static_assert(is_empty<replicate_c_t<N, T>>::value == (sizeof...(U) == 0), "");
+
+        static_assert(size<replicate_c<N, T>>::value == sizeof...(U), "");
+        static_assert(size<replicate_c_t<N, T>>::value == sizeof...(U), "");
+
+        static_assert(equal<
+            vector<U...>,
+            unpack_t<replicate_c<N, T>, into<vector>>
+        >::value, "");
+
+        static_assert(has_optimization<
+            replicate_c<N, T>, optimization::O1_size
+        >::value, "");
     };
 };
 
