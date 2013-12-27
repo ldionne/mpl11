e90b20ec6b7889bf5d11cef1d99427c576faba3b
Louis Dionne <ldionne.2@gmail.com>
2013-12-27 13:20:06 -0500

    Optimize and_ and or_ for integral_c.

--- include/boost/mpl11/v2/integral_c.hpp
+++ include/boost/mpl11/v2/integral_c.hpp
@@ -8,6 +8,9 @@
 
 #include <boost/mpl11/fwd/integral_c.hpp>
 
+#include <boost/mpl11/fwd/and.hpp>
+#include <boost/mpl11/fwd/or.hpp>
+
 
 namespace boost { namespace mpl11 {
     template <typename T, T v>
@@ -18,6 +21,30 @@ namespace boost { namespace mpl11 {
         constexpr operator() const { return value; }
         using type = integral_c;
     };
+
+    namespace integral_c_detail {
+        template <typename ...T>
+        struct or_impl : true_ { };
+
+        template <template <bool B> class ...Bool>
+        struct or_impl<Bool<false>...> : false_ { };
+
+        template <typename ...T>
+        struct and_impl : false_ { };
+
+        template <template <bool B> class ...Bool>
+        struct or_impl<Bool<true>...> : true_ { };
+    } // end namespace integral_c_detail
+
+    template <typename T, T ...v>
+    struct or_<integral_c<T, v>...>
+        : integral_c_detail::or_impl<bool_<static_cast<bool>(v)>...>
+    { };
+
+    template <typename T, T ...v>
+    struct and_<integral_c<T, v>...>
+        : integral_c_detail::and_impl<bool_<static_cast<bool>(v)>...>
+    { };
 }} // end namespace boost::mpl11
 
 #endif // !BOOST_MPL11_INTEGRAL_C_HPP
