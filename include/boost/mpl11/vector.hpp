bc5b03363fadffa5b9c78ec4da2204b7276d4333
Louis Dionne <ldionne.2@gmail.com>
2013-12-31 12:58:23 -0500

    Move detail::variadic_last.

--- include/boost/mpl11/vector.hpp
+++ include/boost/mpl11/vector.hpp
@@ -25,123 +25,123 @@
 
 
 namespace boost { namespace mpl11 {
-namespace vector_detail {
-    template <detail::std_size_t Index, typename Value>
-    struct index_holder { };
+    namespace vector_detail {
+        template <detail::std_size_t Index, typename Value>
+        struct index_holder { };
 
-    template <typename Indices, typename ...T>
-    struct index_based_lookup;
+        template <typename Indices, typename ...T>
+        struct index_based_lookup;
 
-    template <detail::std_size_t ...Indices, typename ...T>
-    struct index_based_lookup<detail::index_sequence<Indices...>, T...>
-        : index_holder<Indices, T>...
+        template <detail::std_size_t ...Indices, typename ...T>
+        struct index_based_lookup<detail::index_sequence<Indices...>, T...>
+            : index_holder<Indices, T>...
+        { };
+    } // end namespace vector_detail
+
+    template <typename ...T>
+    struct vector
+        : vector_detail::index_based_lookup<
+            typename detail::make_index_sequence<sizeof...(T)>::type, T...
+        >
     { };
-} // end namespace vector_detail
 
-template <typename ...T>
-struct vector
-    : vector_detail::index_based_lookup<
-        typename detail::make_index_sequence<sizeof...(T)>::type, T...
-    >
-{ };
+    template <typename ...T>
+    struct tag_of<vector<T...>> {
+        using type = random_access_sequence_tag;
+    };
 
-template <typename ...T>
-struct tag_of<vector<T...>> {
-    using type = random_access_sequence_tag;
-};
+    template <typename ...T>
+    struct sequence_traits<vector<T...>> : defaults::sequence_traits {
+        static constexpr bool has_O1_size = true;
+        static constexpr bool has_O1_unpack = true;
+        static constexpr bool is_finite = true;
+    };
 
-template <typename ...T>
-struct sequence_traits<vector<T...>> : defaults::sequence_traits {
-    static constexpr bool has_O1_size = true;
-    static constexpr bool has_O1_unpack = true;
-    static constexpr bool is_finite = true;
-};
+    /////////////////////////////////
+    // ForwardSequence
+    /////////////////////////////////
+    template <typename Head, typename ...Tail>
+    struct head<vector<Head, Tail...>> {
+        using type = Head;
+    };
 
-/////////////////////////////////
-// ForwardSequence
-/////////////////////////////////
-template <typename Head, typename ...Tail>
-struct head<vector<Head, Tail...>> {
-    using type = Head;
-};
+    template <typename Head, typename ...Tail>
+    struct tail<vector<Head, Tail...>> {
+        using type = vector<Tail...>;
+    };
 
-template <typename Head, typename ...Tail>
-struct tail<vector<Head, Tail...>> {
-    using type = vector<Tail...>;
-};
+    template <typename ...T>
+    struct is_empty<vector<T...>>
+        : false_
+    { };
 
-template <typename ...T>
-struct is_empty<vector<T...>>
-    : false_
-{ };
-
-template <>
-struct is_empty<vector<>>
-    : true_
-{ };
+    template <>
+    struct is_empty<vector<>>
+        : true_
+    { };
 
 
-/////////////////////////////////
-// FiniteSequence
-/////////////////////////////////
-template <typename ...T>
-struct length<vector<T...>>
-    : size_t<sizeof...(T)>
-{ };
+    /////////////////////////////////
+    // FiniteSequence
+    /////////////////////////////////
+    template <typename ...T>
+    struct length<vector<T...>>
+        : size_t<sizeof...(T)>
+    { };
 
-template <typename ...T, typename F>
-struct unpack<vector<T...>, F>
-    : apply<F, T...>
-{ };
-
-
-/////////////////////////////////
-// BidirectionalSequence
-/////////////////////////////////
-template <typename Head, typename ...Tail>
-struct last<vector<Head, Tail...>>
-    : detail::variadic_last<Head, Tail...>
-{ };
-
-template <typename Head, typename ...Tail>
-struct init<vector<Head, Tail...>>
-    : apply<
-        detail::variadic_take<sizeof...(Tail), into<vector>>,
-        Head, Tail...
-    >
-{ };
+    template <typename ...T, typename F>
+    struct unpack<vector<T...>, F>
+        : apply<F, T...>
+    { };
 
 
-/////////////////////////////////
-// RandomAccessSequence
-/////////////////////////////////
-namespace vector_detail {
-    template <detail::std_size_t Index, typename Value>
-    identity<Value> at_index(index_holder<Index, Value>*);
-}
+    /////////////////////////////////
+    // BidirectionalSequence
+    /////////////////////////////////
+    template <typename Head, typename ...Tail>
+    struct last<vector<Head, Tail...>>
+        : detail::variadic_last<Head, Tail...>
+    { };
 
-template <typename ...T, typename Index>
-struct at<vector<T...>, Index>
-    : private BOOST_MPL11_CHECK_USAGE(at<vector<T...>, Index>)
-{
-    using type = typename decltype(
-        vector_detail::at_index<Index::value>((vector<T...>*)nullptr)
-    )::type;
-};
+    template <typename Head, typename ...Tail>
+    struct init<vector<Head, Tail...>>
+        : apply<
+            detail::variadic_take<sizeof...(Tail), into<vector>>,
+            Head, Tail...
+        >
+    { };
 
-template <typename ...T, typename Start, typename Stop>
-struct slice<vector<T...>, Start, Stop>
-    : private BOOST_MPL11_CHECK_USAGE(slice<vector<T...>, Start, Stop>)
-{
-    using type = typename apply<
-        detail::variadic_drop<Start::value,
-            detail::variadic_take<Stop::value - Start::value,
-                into<vector>
-            >
-        >,
-        T...
-    >::type;
-};
+
+    /////////////////////////////////
+    // RandomAccessSequence
+    /////////////////////////////////
+    namespace vector_detail {
+        template <detail::std_size_t Index, typename Value>
+        identity<Value> at_index(index_holder<Index, Value>*);
+    }
+
+    template <typename ...T, typename Index>
+    struct at<vector<T...>, Index>
+        : private BOOST_MPL11_CHECK_USAGE(at<vector<T...>, Index>)
+    {
+        using type = typename decltype(
+            vector_detail::at_index<Index::value>((vector<T...>*)nullptr)
+        )::type;
+    };
+
+    template <typename ...T, typename Start, typename Stop>
+    struct slice<vector<T...>, Start, Stop>
+        : private BOOST_MPL11_CHECK_USAGE(slice<vector<T...>, Start, Stop>)
+    {
+        using type = typename apply<
+            detail::variadic_drop<Start::value,
+                detail::variadic_take<Stop::value - Start::value,
+                    into<vector>
+                >
+            >,
+            T...
+        >::type;
+    };
 }} // end namespace boost::mpl11
 
 #endif // !BOOST_MPL11_VECTOR_HPP
