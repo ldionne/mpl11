53da2829d660dd5792d42e6754c9370234ec190a
Louis Dionne <ldionne.2@gmail.com>
2014-02-03 14:54:36 -0500

    Remove Doxygen group Tags.

--- include/boost/mpl11.hpp
+++ include/boost/mpl11.hpp
@@ -13,50 +13,9 @@
  */
 
 /*!
- * @defgroup typeclasses Type classes
+ * @defgroup typeclasses Typeclasses
  *
- * General purpose type classes provided by the library.
- */
-
-/*!
- * @defgroup tags Tags
- *
- * Tags are types used to dispatch methods.
- *
- * Tags come in two flavors (the difference is only their purpose):
- *
- * ### Typeclass tags
- * A typeclass tag represents a single typeclass. These tags are used as a
- * meeting point to dispatch multi-ary methods with different datatype tags
- * by using the following technique:
- *
- * The typeclass must be user-specialized on the left datatype tag and
- * that specialization must be of the form:
-
-        template <typename RightTag>
-        struct Typeclass<my_left_datatype_tag, RightTag>
-            : Typeclass<the_typeclass_tag, RightTag>
-        { };
-
- * Then, the typeclass should be library-specialized in the following way:
-
-        template <typename RightTag>
-        struct Typeclass<the_typeclass_tag, RightTag>
-            : Typeclass<RightTag, the_typeclass_tag>
-        { };
-
- * This allows `RightTag` to undergo the same process as
- * `my_left_datatype_tag`, so that the typeclass is eventually resolved to
-
-        template <>
-        struct Typeclass<the_typeclass_tag, the_typeclass_tag>;
-
- * Which is the typeclass that implements the method we were searching for.
- *
- *
- * ### Datatype tags
- * A datatype tag represents a specific datatype. These tags are only used to
- * make the datatype an instance of typeclasses.
+ * General purpose typeclasses provided by the library.
  */
 
 /*!
