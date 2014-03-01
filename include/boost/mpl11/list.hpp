7b3fd263710f6d601dca9b826b8ae47b8ca7aecf
Louis Dionne <ldionne.2@gmail.com>
2014-03-01 12:13:16 -0500

    Implement at for List in a more efficient manner.

--- include/boost/mpl11/list.hpp
+++ include/boost/mpl11/list.hpp
@@ -9,10 +9,30 @@
 #include <boost/mpl11/fwd/list.hpp>
 
 #include <boost/mpl11/bool.hpp>
+#include <boost/mpl11/detail/std_index_sequence.hpp>
+#include <boost/mpl11/detail/std_size_t.hpp>
 #include <boost/mpl11/iterable.hpp>
 
 
 namespace boost { namespace mpl11 {
+    namespace list_detail {
+        using namespace detail;
+
+        template <std_size_t n, typename x>
+        struct pair { };
+
+        template <typename ns, typename ...xs>
+        struct index_map;
+
+        template <std_size_t ...ns, typename ...xs>
+        struct index_map<std_index_sequence<ns...>, xs...>
+            : pair<ns, xs>...
+        { };
+
+        template <std_size_t n, typename x>
+        x at_index(pair<n, x>*);
+    } // end namespace list_detail
+
     template <typename x, typename ...xs>
     struct list<x, xs...> {
         using type = list;
@@ -51,6 +71,21 @@ namespace boost { namespace mpl11 {
 
         template <typename self>
         using is_empty_impl = typename self::type::is_empty;
+
+        template <typename n, typename self>
+        struct at_impl : default_Iterable::at_impl<n, self> { };
+
+        template <typename n, typename ...xs>
+        struct at_impl<n, list<xs...>>
+            : decltype(list_detail::at_index<n::type::value>(
+                (list_detail::index_map<
+                    typename detail::make_std_index_sequence<
+                        sizeof...(xs)
+                    >::type,
+                    xs...
+                >*)nullptr
+            ))
+        { };
     };
 }} // end namespace boost::mpl11
 
